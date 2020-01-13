d27fc68c99c9687c55c46c9466e0f7bfb9de2b5c
DivOverlow <ro280367avi@gmail.com>
2020-01-14 01:28:57 +0200

    P14 - Update Mini-Cart

--- resources/themes/custom/views/checkout/cart/mini-cart.blade.php
+++ resources/themes/custom/views/checkout/cart/mini-cart.blade.php
@@ -47,7 +47,7 @@
         <div slot="body">
             <div class="cart-content my-3">
                 @foreach ($items as $item)
-                    <div class="item w-full flex flex-row justify-content-between items-center px-3 py-1 inline-block">
+                    <div class="item w-full flex flex-row items-center px-3 py-1 inline-block">
                         <div class="item-image w-1/5">
                             <?php
                             if ($item->type == "configurable")
@@ -116,63 +116,62 @@
                     </div>
 
                 @endforeach
-            </div>
 
-            <div class="w-full absolute inset-x-0 bottom-0">
-            <?php $gift_products = $giftRepository->getGiftsProduct(); ?>
+                <div class="w-full mt-20">
+                    <?php $gift_products = $giftRepository->getGiftsProduct(); ?>
 
-                @if (count($gift_products))
-                    @inject ('productImageHelper', 'Webkul\Product\Helpers\ProductImage')
-                    <div class="gift-content my-3 pr-3">
-                        @if (session()->has('gift_product_id'))
-                            @foreach($gift_products as $gift_product)
+                    @if (count($gift_products))
+                        @inject ('productImageHelper', 'Webkul\Product\Helpers\ProductImage')
+                        <div class="gift-content my-3 pr-3">
+                            @if (session()->has('gift_product_id'))
+                                @foreach($gift_products as $gift_product)
 
-                                @if(isset($gift_product->related_products()->first()->product_id))
-                                    <?php
+                                    @if(isset($gift_product->related_products()->first()->product_id))
+                                        <?php
                                         $product = $productRepository->find($gift_product->related_products()->first()->product_id);
                                         $productBaseImage = $productImageHelper->getProductBaseImage($product);
-                                    ?>
+                                        ?>
 
-                                    @if (session()->get('gift_product_id') == $product->product_id)
-                                        <div class="w-full flex flex-row items-center justify-between text-left py-1 border-t-2 border-orange-500 rounded-b shadow-md">
-                                            <div class="item-image h-28 w-1/3 flex items-center justify-center">
-                                                <a href="{{ url()->to('/').'/products/'.$product->url_key }}"><img  class="object-scale-down h-24 w-auto"
-                                                                                                                    src="{{ $productBaseImage['small_image_url'] }}"/></a>
-                                            </div>
-                                            <div class="item-title flex flex-col items-start justify-around h-24">
-                                                <div class="w-4/5 ml-auto tracking-widest text-gold">{{ __('shop::app.checkout.gift.title') }}</div>
-                                                <div class="text-base text-gray-dark uppercase hover:text-gray-cloud">
-                                                    <a href="{{ url()->to('/').'/products/'.$product->url_key }}">
-                                                        {{ $product->name }} </a>
+                                        @if (session()->get('gift_product_id') == $product->product_id)
+                                            <div class="w-full flex flex-row items-center justify-between text-left py-2 border-t-2 border-orange-500 rounded-b shadow-md">
+                                                <div class="item-image h-28 w-1/3 flex items-center justify-center">
+                                                    <a href="{{ url()->to('/').'/products/'.$product->url_key }}"><img  class="object-scale-down h-24 w-auto"
+                                                                                                                        src="{{ $productBaseImage['small_image_url'] }}"/></a>
                                                 </div>
-                                                <div class="font-serif font-medium text-base text-gray-cloud">
-                                                    {{  __('shop::app.checkout.gift.free') }}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    @else
-                                        <div class="w-full flex flex-row justify-between items-center text-left py-1">
-                                            <div class="item-image h-28 w-1/2 flex items-center justify-center">
-                                                <a href="{{ url()->to('/').'/products/'.$product->url_key }}"><img  class="object-scale-down h-24 w-auto"
-                                                                                                                    src="{{ $productBaseImage['small_image_url'] }}"/></a>
-                                            </div>
-                                            <div class="item-title flex flex-col justify-start">
-                                                <div class="text-base text-gray-dark uppercase hover:text-gray-cloud">
-                                                    <a href="{{ url()->to('/').'/products/'.$product->url_key }}">
-                                                        {{ $product->name }} </a>
-                                                </div>
-                                                <div class="font-serif font-medium text-base text-gray-cloud">
-                                                    {{ ($cart->base_sub_total < $gift_product->action_amount) ? __('shop::app.checkout.gift.premium') : __('shop::app.checkout.gift.free') }}
-                                                </div>
-                                                <div class="font-normal font-medium text-base text-gray-cloud leading-tight">
-                                                    {{ ($cart->base_sub_total < $gift_product->action_amount) ? sprintf(__('shop::app.checkout.gift.premium-message'), core()->convertPrice($gift_product->action_amount - $cart->base_sub_total) . core()->currencySymbol(core()->getBaseCurrencyCode()) ) : __('shop::app.checkout.gift.free-message') }}
+                                                <div class="item-title flex flex-col items-start justify-around h-28">
+                                                    <div class="w-4/5 ml-auto tracking-widest text-gold">{{ __('shop::app.checkout.gift.title') }}</div>
+                                                    <div class="text-base text-gray-dark uppercase hover:text-gray-cloud">
+                                                        <a href="{{ url()->to('/').'/products/'.$product->url_key }}">
+                                                            {{ $product->name }} </a>
+                                                    </div>
+                                                    <div class="font-serif font-medium text-base text-gray-cloud">
+                                                        {{  __('shop::app.checkout.gift.free') }}
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        @else
+                                            <div class="w-full flex flex-row justify-between items-center text-left py-2">
+                                                <div class="item-image h-28 w-1/2 flex items-center justify-center">
+                                                    <a href="{{ url()->to('/').'/products/'.$product->url_key }}"><img  class="object-scale-down h-24 w-auto"
+                                                                                                                        src="{{ $productBaseImage['small_image_url'] }}"/></a>
+                                                </div>
+                                                <div class="item-title flex flex-col justify-start">
+                                                    <div class="text-base text-gray-dark uppercase hover:text-gray-cloud">
+                                                        <a href="{{ url()->to('/').'/products/'.$product->url_key }}">
+                                                            {{ $product->name }} </a>
+                                                    </div>
+                                                    <div class="font-serif font-medium text-base text-gray-cloud">
+                                                        {{ ($cart->base_sub_total < $gift_product->action_amount) ? __('shop::app.checkout.gift.premium') : __('shop::app.checkout.gift.free') }}
+                                                    </div>
+                                                    <div class="font-normal font-medium text-base text-gray-cloud leading-tight">
+                                                        {{ ($cart->base_sub_total < $gift_product->action_amount) ? sprintf(__('shop::app.checkout.gift.premium-message'), core()->convertPrice($gift_product->action_amount - $cart->base_sub_total) . core()->currencySymbol(core()->getBaseCurrencyCode()) ) : __('shop::app.checkout.gift.free-message') }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endif
                                     @endif
-                                @endif
-                            @endforeach
-                        @else
+                                @endforeach
+                            @else
                                 <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
                                     <p>{{__('shop::app.checkout.gift.gift-not-available')}}</p>
                                 </div>
@@ -206,36 +205,39 @@
                                     @endif
                                 @endforeach
 
-                        @endif
+                            @endif
 
-                    </div>
-                @endif
-
-                <div class="cart-footer">
-                    <div class="bg-gray-snow w-full font-medium text-gray-dark h-16 flex content-center flex-wrap">
-                        <div class="w-2/3 text-center uppercase">
-                            {{ __('shop::app.checkout.cart.cart-subtotal') }}:
                         </div>
-                        <div class="w-1/3 text-center">
-                            {!! view_render_event('bagisto.shop.checkout.cart-mini.subtotal.before', ['cart' => $cart]) !!}
+                    @endif
+                </div>
 
-                            {{ core()->currency($cart->base_sub_total) }}
+            </div>
 
-                            {!! view_render_event('bagisto.shop.checkout.cart-mini.subtotal.after', ['cart' => $cart]) !!}
-                        </div>
+            <div class="cart-footer absolute inset-x-0 bottom-0">
+                <div class="bg-gray-snow w-full font-medium text-gray-dark h-16 flex content-center flex-wrap">
+                    <div class="w-2/3 text-center uppercase">
+                        {{ __('shop::app.checkout.cart.cart-subtotal') }}:
                     </div>
-                    <div class="w-full flex flex-row items-center text-sm sm:text-base">
+                    <div class="w-1/3 text-center">
+                        {!! view_render_event('bagisto.shop.checkout.cart-mini.subtotal.before', ['cart' => $cart]) !!}
+
+                        {{ core()->currency($cart->base_sub_total) }}
+
+                        {!! view_render_event('bagisto.shop.checkout.cart-mini.subtotal.after', ['cart' => $cart]) !!}
+                    </div>
+                </div>
+                <div class="w-full flex flex-row items-center text-sm sm:text-base">
                         <span class="button-black w-full py-3 normal-case">
 {{--                            <a href="{{ route('shop.checkout.cart.index') }}">{{ __('shop::app.checkout.cart.continue-shopping') }}</a>--}}
                             <a href="{{ route('shop.checkout.cart.index') }}">{{ __('shop::app.minicart.view-cart') }}</a>
                         </span>
-                        <span class="button-decor w-full py-3 normal-case">
+                    <span class="button-decor w-full py-3 normal-case">
 {{--                            <a href="{{ route('shop.checkout.cart.index') }}">{{ __('shop::app.minicart.view-cart') }}</a>--}}
                             <a  href="{{ route('shop.checkout.onepage.index') }}">{{ __('shop::app.minicart.checkout') }}</a>
                         </span>
-                    </div>
                 </div>
             </div>
+
         </div>
     </card-modal>
 @else
