0ab3519ce105051314868f07e38c88654244b24b
DivOverlow <ro280367avi@gmail.com>
2020-01-30 22:02:34 +0200

    P30- Page Static On

--- resources/themes/custom/views/emails/sales/new-order.blade.php
+++ resources/themes/custom/views/emails/sales/new-order.blade.php
@@ -5,8 +5,8 @@
 
 <div style="background-color: #282828; width: 100%">
     <div style="width: 100%;max-width: 600px;margin:auto;">
-        <div style="height: 170px;background-color: #212121;padding: 0 30px;">
-            <div style="height: 90px;text-align: center;">
+        <div style="display:flex;align-content: space-around;flex-wrap: wrap;height: 170px;background-color: #212121;padding: 0 30px;">
+            <div style="width:100%;height:90px;display:flex;justify-content: center;">
                 <a style="text-decoration: none;color: #fff;" href="{{ config('app.url') }}">
                     @include ('shop::emails.layouts.logo')
                 </a>
@@ -14,22 +14,22 @@
 
 
             <div style="display:flex;letter-spacing:0.05em;width:100%;background-color:#212121;">
-                <ul style="padding-inline-start: 0 !important;display:flex;align-items: center;width:100%;font:inherit;font-size:1rem;font-weight:500;text-transform:uppercase">
-                    <li style="list-style: none;margin:0;padding:0;border:0;text-align: center;width:23%;border-right:1px solid #FFF">
+                <div style="display:flex;align-items: center;width:100%;font:inherit;font-size:1rem;font-weight:500;text-transform:uppercase">
+                    <div style="text-align: center;width:23%;border-right:1px solid #FFF">
                         <a style="text-decoration: none;color: #fff;" href="{{ route('shop.categories.index', 'products') }}">{{ __('shop::app.mail.order.menu.products') }}</a>
-                    </li>
-                    <li style="list-style: none;margin:0;padding:0;border:0;text-align: center;width:23%;border-right:1px solid #FFF">
+                    </div>
+                    <div style="text-align: center;width:23%;border-right:1px solid #FFF">
                         <a style="text-decoration: none;color: #fff;" href="{{ route('shop.categories.index', 'collections') }}">{{ __('shop::app.mail.order.menu.collections') }}</a>
-                    </li>
-                    <li style="list-style: none;margin:0;padding:0;border:0;text-align: center;width:23%;border-right:1px solid #FFF">
+                    </div>
+                    <div style="text-align: center;width:23%;border-right:1px solid #FFF">
                         <a style="text-decoration: none;color: #fff;" href="{{ route('shop.categories.index', 'gifts') }}">{{ __('shop::app.mail.order.menu.gifts') }}</a>
-                    </li>
-                    <li style="list-style: none;margin:0;padding:0;border:0;text-align: center;width:23%;border-right:1px solid #FFF">
+                    </div>
+                    <div style="text-align: center;width:23%;border-right:1px solid #FFF">
                         <a style="text-decoration: none;color: #fff;" href="{{ route('shop.cms.page', 'about-us') }}">{{ __('shop::app.mail.order.menu.about-us') }}</a>
-                    </li>
-                    <li style="list-style: none;margin:0;padding:0;border:0;text-align: center;width: 8%"><a style="text-decoration: none;color: #fff;" href="{{ route('customer.session.index') }}">
-                            <span style="margin-bottom: -8px;width: 27px;height: 27px;display: inline-block;background-image: url({{ bagisto_asset('images/user.png') }});background-position: top center;background-repeat: no-repeat;margin-top: -2px;"></span></a></li>
-                </ul>
+                    </div>
+                    <div style="text-align: center;width: 8%"><a style="text-decoration: none;color: #fff;" href="{{ route('customer.session.index') }}">
+                            <span style="margin-bottom: -8px;width: 27px;height: 27px;display: inline-block;background-image: url({{ bagisto_asset('images/user.png') }});background-position: top center;background-repeat: no-repeat;margin-top: -2px;"></span></a></div>
+                </div>
             </div> <!-- end top-nav -->
         </div>
 
@@ -181,7 +181,7 @@
 
         <div class="section-footer" style="background-color: #2f2f2f;display: flex;flex-direction: column;height: 688px;padding: 0 20px;">
             <div style="display: flex;align-items: center;justify-content: center;width: 100%;border-bottom: solid 1px #727272;height: 160px;">
-                <ul style="padding-inline-start: 0 !important;display: flex;justify-content: space-around;width: 90%;">
+                <ul style="display: flex;justify-content: space-around;width: 90%;">
                     <li style="list-style: none !important;margin: 0;padding: 0;border: 0;font-size: 100%;font: inherit;"><a style="text-decoration: none;color: #fff;" href="#" target="_blank" rel="noopener noreferrer">
                             <svg width="40" height="40" fill="#979797" xmlns="http://www.w3.org/2000/svg"
                                  viewBox="0 0 24 24">
@@ -225,7 +225,7 @@
 
                 <div class="list-link" style="display: flex;justify-content: space-around;width: 100%;font-size: 1rem;">
                     <div style="width: 35%;">
-                        <ul style="padding-inline-start: 0 !important;display: flex;flex-direction: column;line-height: 2;">
+                        <ul style="display: flex;flex-direction: column;line-height: 2;">
                             <li style="list-style: none !important;margin: 0;padding: 0;border: 0;font-size: 100%;font: inherit;color: #fff;">Информация</li>
                             <li style="list-style: none !important;margin: 0;padding: 0;border: 0;font-size: 100%;font: inherit;"><a style="text-decoration: none;color: #969696;" href="@php echo route('shop.cms.page', 'delivery-and-payment') @endphp">Доставка и оплата</a></li>
                             <li style="list-style: none !important;margin: 0;padding: 0;border: 0;font-size: 100%;font: inherit;"><a style="text-decoration: none;color: #969696;" href="@php echo route('shop.cms.page', 'faq') @endphp">Частые вопросы</a></li>
@@ -233,7 +233,7 @@
                         </ul>
                     </div>
                     <div style="width: 35%;">
-                        <ul style="padding-inline-start: 0 !important;display: flex;flex-direction: column;line-height: 2;">
+                        <ul style="display: flex;flex-direction: column;line-height: 2;">
                             <li style="list-style: none !important;margin: 0;padding: 0;border: 0;font-size: 100%;font: inherit;color: #fff;">О бренде</li>
                             <li style="list-style: none !important;margin: 0;padding: 0;border: 0;font-size: 100%;font: inherit;"><a style="text-decoration: none;color: #969696;" href="@php echo route('shop.cms.page', 'about-us') @endphp">О нас</a></li>
                             <li style="list-style: none !important;margin: 0;padding: 0;border: 0;font-size: 100%;font: inherit;"><a style="text-decoration: none;color: #969696;" href="@php echo route('shop.cms.page', 'leaving') @endphp">Уход с Mamash</a></li>
@@ -246,7 +246,7 @@
             <div style="display: flex;align-content: space-around;justify-content: center;flex-wrap: wrap;width: 100%;height: 195px;">
 
                 <div style="height: 33px;display: inline-block;background-image: url({!! bagisto_asset('images/cards.png') !!});background-position: top center;background-repeat: no-repeat;width: 100%"></div>
-                <ul style="padding-inline-start: 0 !important;display: flex;">
+                <ul style="display: flex;">
                     <li style="list-style: none !important;margin: 0;padding: 0;border: 0;font-size: 100%;font: inherit;margin-right: 2rem;">
                         <a style="text-decoration: none;color: #fff;" href="@php echo route('shop.cms.page', 'terms-conditions') @endphp">Условия покупки</a>
                     </li>
